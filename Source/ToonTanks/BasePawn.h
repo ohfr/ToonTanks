13b12fe35ef75ea1c784786ca2a67a449c23dab2
Daniel Martin <danielmartin1205@gmail.com>
2021-11-28 23:03:37 -0500

    initial commit

---
+++ Source/ToonTanks/BasePawn.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite);
+	int32 VisibleAnywhereInt = 12;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly);
+	int32 EditAnywhereInt = 22;
+
+	UPROPERTY(VisibleDefaultsOnly);
+	int32 VisibleDefaultsOnlyInt = 5;
+
+	UPROPERTY(VisibleInstanceOnly);
+	int32 VisibleInstanceOnlyInt = 11;
+
+	UPROPERTY(EditDefaultsOnly);
+	int32 EditDefaultsOnlyInt = 9;
+
+	UPROPERTY(EditInstanceOnly);
+	int32 EditInstanceOnlyInt = 14;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite);
+	float Speed = 400.f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
